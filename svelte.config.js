c6359c9593b6c6d30f3d249e9f2900c6bbb0be73
NIELS BAKKER <nielsbakker04@gmail.com>
2024-11-25 16:32:30 +0100

    terug naar originele config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
-import vercel from '@sveltejs/adapter-vercel';
-
+import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// Gebruik de Vercel-adapter
-		adapter: vercel({
-			runtime: 'nodejs18.x' // Gebruik Node.js 18
-		})
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
-
 export default config;
